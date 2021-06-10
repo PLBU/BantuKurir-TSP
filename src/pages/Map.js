7849b97f7926f62d610caa6060cdd9cacc3581cd
Renaldi Arlin <johanes123renaldi@gmail.com>
2021-06-10 22:03:54 +0700

    Fixed physics graph:

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -13,7 +13,7 @@ import '../styles/map.css'
 export default (props) => {
     const colors = ["crimson", "turquoise", "darkgreen", "coral", "cadetblue", "burlywood"]
     const { ans } = props.location.state
-    
+
     const history = useHistory()
     const [graph, setGraph] = React.useState(null)
 
@@ -30,24 +30,33 @@ export default (props) => {
         let nodes = []
         let prev = 0
         for (const [index, el] of ans.entries()) {
-            if (index >= colors.length) colors.push("#" + ((1<<24)*Math.random() | 0).toString(16));
+            if (index >= colors.length) colors.push("#" + ((1 << 24) * Math.random() | 0).toString(16));
             for (let i = 0; i < el.cost.length; i++) {
                 for (let j = 0; j < el.cost[0].length; j++) {
-                    if (j < i) edges.push({
-                        from: prev + i,
-                        to: prev + j,
-                        arrows: { to: false },
-                        label: el.cost[i][j].toFixed(2).toString(),
-                    })
+                    if (j < i) {
+                        const prevNodeI = nodes.find(elmt => (elmt.label === el.listOfLoc[i].name && elmt.x === el.listOfLoc[i].x * 200 && elmt.y === el.listOfLoc[i].y * 200));
+                        const prevNodeJ = nodes.find(elmt => (elmt.label === el.listOfLoc[j].name && elmt.x === el.listOfLoc[j].x * 200 && elmt.y === el.listOfLoc[j].y * 200));
+
+                        edges.push({
+                            from: (!prevNodeI) ? prev + i : prevNodeI.id,
+                            to: (!prevNodeJ) ? prev + j : prevNodeJ.id,
+                            arrows: { to: false },
+                            label: el.cost[i][j].toFixed(2).toString(),
+                        })
+                    }
                 }
             }
-            
+
             for (let i = 0; i < el.path.length - 1; i++) {
-                const from = el.path[i] + prev;
-                const to = el.path[i + 1] + prev;
-    
+                const idFrom =  el.path[i]
+                const idTo =  el.path[i + 1]
+                const prevNodeFrom = nodes.find(elmt => (elmt.label === el.listOfLoc[idFrom].name && elmt.x === el.listOfLoc[idFrom].x * 200 && elmt.y === el.listOfLoc[idFrom].y * 200));
+                const prevNodeTo = nodes.find(elmt => (elmt.label === el.listOfLoc[idTo].name && elmt.x === el.listOfLoc[idTo].x * 200 && elmt.y === el.listOfLoc[idTo].y * 200));
+                const from = (prevNodeFrom) ? prevNodeFrom.id : idFrom + prev;
+                const to = (prevNodeTo) ? prevNodeTo.id : idTo + prev;
+
                 const idx = edges.findIndex((value) => ((value.from === from && value.to === to) || (value.from === to && value.to === from)))
-    
+
                 edges[idx] = {
                     ...edges[idx],
                     color: colors[index],
@@ -57,16 +66,18 @@ export default (props) => {
 
             let newNodesCount = 0
             for (const [i, val] of el.listOfLoc.entries()) {
-                if (!nodes.some(elmt => (elmt.label === val.label && elmt.x === val.x && elmt.y === val.y))) {
+                if (!nodes.some(elmt => (elmt.label === val.name && elmt.x === val.x * 200 && elmt.y === val.y * 200))) {
                     nodes.push({
-                        id: prev + i, 
+                        id: prev + i,
                         label: val.name,
-                        x: val.x * 200, 
-                        y: val.y * 200, 
+                        x: val.x * 200,
+                        y: val.y * 200,
                         title: `Coordinate: (${val.x},${val.y})`,
-                        color: (i === 0) ? colors[index] : "lightgray"
+                        color: (i === 0) ? colors[index] : "lightgray",
+                        font: {
+                            face: 'roboto'
+                        }
                     })
-
                     newNodesCount++
                 }
             }
@@ -82,12 +93,11 @@ export default (props) => {
     }
 
     React.useEffect(() => {
-        console.log(ans)
         setUpGraph()
     }, [])
 
     return (
-        <div style={{ display: 'flex', flexDirection: 'column', height: '100vh', justifyContent: 'center'}}>
+        <div style={{ display: 'flex', flexDirection: 'column', height: '100vh', justifyContent: 'center' }}>
             <Paper style={{ margin: 'auto', width: '1024px', padding: 24 }}>
                 <Typography color="textSecondary" gutterBottom>
                     <code>
@@ -100,7 +110,7 @@ export default (props) => {
                     options={options} />}
             </Paper>
             <div className={"slide-container"}>
-                {graph && ans.map((value, index) => 
+                {graph && ans.map((value, index) =>
                     <MyCard
                         style={{ flex: '0 0 auto', width: 500, margin: 24 }}
                         heading={`Shipped at ${value.date}, ${value.time}`}
@@ -111,7 +121,7 @@ export default (props) => {
                         } />)}
             </div>
             <Fab color="primary"
-                style={{ position: "fixed", top: 48, left: 48}}
+                style={{ position: "fixed", top: 48, left: 48 }}
                 onClick={() => history.push('/')}>
                 <ArrowBackIcon size="large" />
             </Fab>
